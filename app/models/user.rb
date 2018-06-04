89b6cf1cdac3dc0fb32dcb72218d724db1835e9a
pedrog1997 <pedro_0797@hotmail.com>
2018-06-04 16:29:10 +0200

    login valid test

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,11 @@ class User < ApplicationRecord
   before_save { self.email = email.downcase }
   
   has_secure_password
+
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
